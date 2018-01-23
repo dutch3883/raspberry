4da16f41f2f5cbbe3b283be978c540ab6b986831
dutch3883 <dutch3883@hotmail.com>
2018-01-23 16:45:53 +0000

    add fork process for each session

--- __main__.py
+++ __main__.py
@@ -2,23 +2,42 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import datetime
 import base64
+import os
 from raspberry import api
 def routine():
     # initialize the camera and grab a reference to the raw camera capture
-    camera = PiCamera()
-    camera.vflip = False
-    camera.resolution = (500,500)
-    camera.capture('Image.jpg')
-    with open("Image.jpg","rb") as image_file:
+    with PiCamera() as camera:
+        camera.vflip = True
+        camera.resolution = (500,500)
+        img_name = 'Image'+str(os.getpid())+datetime.datetime.now().strftime('_%Y-%m-%d %H:%M:%S')+'.jpg'
+        camera.capture(img_name)
+    with open(img_name,"rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     time_req = time.time()
     response = api.check_image(encoded_string)
     time_res = time.time()
-    print("have response {} with time= {:.3f} sec".format(response,(time_res-time_req)))
+    if not response:
+        try:
+            os.remove(img_name)
+        except Exception:
+            pass
+    else:
+        os.rename('found/'+img_name)
+    print("pid {} have response {} with time= {:.3f} sec".format(os.getpid(),response,(time_res-time_req)))
+    os._exit(0)
     
 if __name__ == '__main__':
+   for i in range(10):
+        newpid = os.fork()
+        if newpid == 0:
+            routine()
+        else:
+            print("parent: {}, child:{} ".format(os.getpid(), newpid))
+            
+        time.sleep(1)
+       
    
-    routine()
 
 
--- raspberry/api_connector/face_rec_api.py
+++ raspberry/api_connector/face_rec_api.py
@@ -1,12 +1,11 @@
 
 import requests
 #import logger
-#site_url   = 'http://ddfacematch.azurewebsites.net'
-site_url = 'http://172.20.10.2'
+site_url   = 'http://ddfacematch.azurewebsites.net'
+#site_url = 'http://172.20.10.2'
 def check_image(img):
     headers = { 'Content-Type':'application/json'}
     data = {'img':img.decode('utf-8')}
-    print(data)
     url = site_url+'/rest/logic/checkFace'
     res = requests.post(url,headers=headers,json=data)
     #print(str(data).replace('\'','\"'))
